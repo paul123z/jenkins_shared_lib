76e656087d8720e2a6008f3062713ebfb82e5381
Pavol Lantaj <palolantaj@gmail.com>
2023-07-01 14:42:29 +0200

    c

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -3,7 +3,7 @@ def call(String project, String ImageTag, String hubUser){
     sh """
     echo "------ TRIVY STEP RUNNING NOW -------"
     trivy image ${hubUser}/${project}:latest > scan.txt
-    cat  sca.txt
+    cat  scan.txt
     echo "------ TRIVY STEP FINALIZED -------"
     """
 }
