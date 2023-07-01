9f16c1b2d23f8e4510da814ecf45463c625bb3b8
Pavol Lantaj <palolantaj@gmail.com>
2023-07-01 18:46:52 +0200

    fix

--- vars/dockerBuildECR.groovy
+++ vars/dockerBuildECR.groovy
@@ -1,9 +1,6 @@
 def call(String aws_account_id, String region, String ecr_repoName){
 
-    def dateFormat = new SimpleDateFormat("yyyyMMdd-HHmmss")
-    def currentDate = dateFormat.format(new Date())
-    def imageTag = "${currentDate}-${env.BUILD_ID}"
-
+  
 sh """
 docker build -t ${ecr_repoName} .
 docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
