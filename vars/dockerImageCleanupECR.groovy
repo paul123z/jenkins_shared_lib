d45e0d93c3914c7bc4bf9cc7552857dce2e39fa1
Pavol Lantaj <palolantaj@gmail.com>
2023-07-01 18:40:09 +0200

    CLEANUP

--- vars/dockerImageCleanupECR.groovy
+++ vars/dockerImageCleanupECR.groovy
@@ -1,6 +1,9 @@
 def call(String project, String ImageTag, String hubUser){
 // docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${ImageTag}
+
+
     sh """
     docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${imageTag}
     """
 }
