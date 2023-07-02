4ce4dd5c4b244a9ad9abc3ccfcd7ac72fa677766
Pavol Lantaj <palolantaj@gmail.com>
2023-07-02 17:31:20 +0200

    updating pipeline - removing some shared libraries

--- vars/dockerImageCleanupECR.groovy
+++ vars/dockerImageCleanupECR.groovy
@@ -1,4 +1,4 @@
-def call(String project, String ImageTag, String hubUser){
+def call(String aws_account_id, String region, String ecr_repoName){
 // docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${ImageTag}
 
 
